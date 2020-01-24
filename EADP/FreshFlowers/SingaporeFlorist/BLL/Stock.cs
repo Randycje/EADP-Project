82400bfa29dce1b6ceb8da8f11fa59365ac81164
Randy <randychong.je@gmail.com>
2020-01-25 01:28:17 +0800

    Supplier Changes

---
+++ EADP/FreshFlowers/SingaporeFlorist/BLL/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SingaporeFlorist.SingaporeFlorist;
+using SingaporeFlorist.DAL;
+using System.Data;
+
+namespace SingaporeFlorist.BLL
+{
+    public class Stock
+    {
+        DALFreshFLowers dal = new DALFreshFLowers();
+
+        public DataSet getAllStocks()
+        {
+            return dal.GetAllStocks();
+        }
+    }
+}
---
+++ EADP/FreshFlowers/SingaporeFlorist/Index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SingaporeFlorist.BLL;
+
+namespace SingaporeFlorist
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        Stock stocks = new Stock();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bind();
+        }
+
+        protected void bind()
+        {
+            gvProduct.DataSource = stocks.getAllStocks();
+            gvProduct.DataBind();
+        }
+    }
+}
